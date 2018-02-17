f0d8b45a0c43d07c4c3f8fb802daf6e3460d5e99
Martin Sonnenholzer <chaosmonster.github.io@googlemail.com>
2018-02-17 14:07:22 +0100

    Add TextAnalysisService to use the Azure sentiment API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  azureSentimentServiceUrl: 'https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0',
+  azureTextAnalysisToken: 'a6b28b568f674e99bd445be3b37401c9'
 };
