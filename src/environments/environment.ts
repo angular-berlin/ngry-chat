d4b761674d0eb7ff867e5e743c67ad7ed2574217
Martin Sonnenholzer <chaosmonster.github.io@googlemail.com>
2018-02-17 15:43:46 +0100

    moved all also in the environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  azureSentimentServiceUrl: '',
-  azureTextAnalysisToken: '',
+  azureSentimentServiceUrl: 'https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+  azureTextAnalysisToken: 'a6b28b568f674e99bd445be3b37401c9',
   twilioTokenURL: 'https://camel-hippopotamus-7503.twil.io/chat-token?identity=user&device=web'
 };
